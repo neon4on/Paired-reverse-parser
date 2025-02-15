d758ed2b9cac693d8ebbc3b8c9259ce5aa996de5
Valeriy <valeriy@Mac-mini-Valeriy.local>
2025-02-15 09:35:05 +0300

    feat: Combined the components and made initial props transfer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <GameInput @start="handleStart" />
+    <!-- Передаём параметры N и M в GameBoard, а также получаем его ссылку для прямого вызова метода -->
+    <GameBoard ref="gameBoard" :N="N" :M="M" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GameInput from "./components/GameInput.vue";
+import GameBoard from "./components/GameBoard.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    GameInput,
+    GameBoard,
+  },
+  data() {
+    return {
+      N: 3,
+      M: 3,
+    };
+  },
+  methods: {
+    handleStart({ N, M }) {
+      console.log("App.vue: Received start event with:", N, M);
+      this.N = N;
+      this.M = M;
+      console.log("App.vue: Updated parameters to:", this.N, this.M);
+      // Вызываем метод startGame у компонента GameBoard, чтобы пересоздать игру с новыми параметрами
+      if (this.$refs.gameBoard && this.$refs.gameBoard.startGame) {
+        console.log("App.vue: Calling startGame on GameBoard");
+        this.$refs.gameBoard.startGame();
+      }
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
